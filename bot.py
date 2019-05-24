8d5aab3d15ca748375884a8320985ffddd5455d3
Liam Hockley <xdega121@gmail.com>
2019-05-24 10:28:10 -0500

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os, discord
+from random import randint
+from discord.ext.commands import Bot
+
+BOT_PREFIX = os.environ['discord_prefix']
+TOKEN = os.environ['discord_token']
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    await client.change_presence(game=discord.Game(name="101010"))
+
+client.run(TOKEN)
+
+@client.command(pass_context = True)
+async def roll(ctx):
+    num = randint(1,100)
+    await client.say(ctx.message.author.mention + ' rolled ' + num)
