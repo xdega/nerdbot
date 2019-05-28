7dd50ad361d87738145522146b753155f1f3fe2a
Liam Hockley <xdega121@gmail.com>
2019-05-28 12:24:22 -0500

    Install pylint; Cleanup syntax

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os, discord
+import os
 import requests
 from random import randint
 from discord.ext.commands import Bot
@@ -7,46 +7,46 @@ BOT_PREFIX = os.environ['discord_prefix']
 TOKEN = os.environ['discord_token']
 WEATHER_API_KEY = os.environ['open_weather_map_key']
 
-client = Bot(command_prefix=BOT_PREFIX)
+CLIENT = Bot(command_prefix=BOT_PREFIX)
 
-@client.event
+@CLIENT.event
 async def on_ready():
+    """ Startup Procedure """
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(CLIENT.user.name)
+    print(CLIENT.user.id)
     print('------')
-    await client.change_presence(game=discord.Game(name="101010 >help"))
+    await CLIENT.change_presence(game=discord.Game(name="101010 >help"))
 
 # Remove default help command
-client.remove_command('help')
+CLIENT.remove_command('help')
 
-@client.command(pass_context = True)
+@CLIENT.command(pass_context=True)
 async def help(ctx):
+    """ Sends help message to the user, informing of available commands """
     message = """
 Hey there! You need help?
 Here are the commands I respond to:
 ** >help ** - I will whisper you this help message
 ** >roll ** - Nothing unique. I'll just roll a dice from 1-100. Good Luck!
     """
-    await client.send_message(ctx.message.author, message)
+    await CLIENT.send_message(ctx.message.author, message)
 
-@client.command()
-async def weather(zipcode, country=''):
+@CLIENT.command()
+async def weather(zipcode):
+    """ Gets the current weather for given US zip code """
     url = 'https://api.openweathermap.org/data/2.5/weather?'
     url += 'zip=' + zipcode
-    if(country != ''): url += ',' + country
     url += '&appid=' + WEATHER_API_KEY
-
-    int(str(number)[:2])
 
     response = requests.get(url)
     response = response.json()
 
-    weather = response['weather'][0]['description']
-    forecast = weather['description']
+    weather_data = response['weather'][0]['description']
+    forecast = weather_data['description']
     station = response['name']
 
-    code = weather['id']
+    code = weather_data['id']
     code = int(str(code)[:1])
     emojistr = {
         2: ':thunder_cloud_rain: :fearful:',
@@ -58,17 +58,19 @@ async def weather(zipcode, country=''):
     }
     emojistr = emojistr.get(code, '')
 
-    await client.say('**The Weather for ' + station + 'is: **' + forecast + ' ' + emojistr)
+    await CLIENT.say('**The Weather for ' + station + 'is: **' + forecast + ' ' + emojistr)
 
-@client.command()
+@CLIENT.command()
 async def affix():
+    """ Gets the current Mythic+ affixes """
     response = requests.get("https://raider.io/api/v1/mythic-plus/affixes?region=us&locale=en")
     response = response.json()
     affixes = response['title']
-    await client.say('**Current Mythic+ Affixes (US):** ' + affixes)
+    await CLIENT.say('**Current Mythic+ Affixes (US):** ' + affixes)
 
-@client.command()
+@CLIENT.command()
 async def io(region, realm, player):
+    """ Gets the Raider IO score for region, realm, player """
     # Ensure proper formatting of params
     region = region.lower()
     realm = realm.lower()
@@ -76,21 +78,22 @@ async def io(region, realm, player):
 
     # Build request URL
     url = "https://raider.io/api/v1/characters/profile"
-    url +="?region=" + region
-    url +="&realm=" + realm
-    url +="&name=" + player
-    url +="&fields=mythic_plus_scores_by_season:current"
+    url += "?region=" + region
+    url += "&realm=" + realm
+    url += "&name=" + player
+    url += "&fields=mythic_plus_scores_by_season:current"
 
     response = requests.get(url)
     response = response.json()
     score = response['mythic_plus_scores_by_season'][0]['scores']['all']
     score = str(score)
-    await client.say('**Raider IO score for '+ player + ':** ' + score)
+    await CLIENT.say('**Raider IO score for '+ player + ':** ' + score)
 
-@client.command(pass_context = True)
+@CLIENT.command(pass_context=True)
 async def roll(ctx):
-    num = randint(1,100)
+    """ Rolls a pseudorandom number from 0 to 100 """
+    num = randint(0, 100)
     num = str(num)
-    await client.say(ctx.message.author.mention + ' rolled ' + num)
+    await CLIENT.say(ctx.message.author.mention + ' rolled ' + num)
 
-client.run(TOKEN)
+CLIENT.run(TOKEN)
