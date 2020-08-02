5b71dbdeee244e3cc900983cd8607793fada06ae
Liam Hockley <xdega121@gmail.com>
2020-08-02 16:12:10 -0700

    Set status online

--- bot.py
+++ bot.py
@@ -22,8 +22,8 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    activity = discord.Game(name=BOT_PREFIX + 'help', type=3)
-    await bot.change_presence(status=discord.Status.idle, activity=activity)
+    activity = discord.Game(name=BOT_PREFIX + 'help', type=discord.ActivityType.watching)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
 
 # Remove default help command
 bot.remove_command('help')
