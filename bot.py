e0bb597a39dd4ef65d036158ee38494210c7943f
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2021-01-29 15:00:49 -0600

    Using f string formatting here
    Co-authored-by: Liam Hockley <xdega121@gmail.com>

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ async def prefix(ctx, value):
     """ Allows server administrators to change the bot prefix """
     if ctx.message.author.server_permissions.administrator:
         os.environ['discord_prefix'] = value
-        helpstr = os.environ['discord_prefix'] + 'help'
+        helpstr = f"{os.environ['discord_prefix']}help"
 
         await bot.change_presence(game=discord.Game(name=helpstr, type=3))
     else:
