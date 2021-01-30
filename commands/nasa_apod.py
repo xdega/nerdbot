9b4efaf2cc538adf42c9b00d6ced53b1bf950cde
Matthew J. DeGarmo <matthewjdegarmo@gmail.com>
2021-01-29 20:53:26 -0600

    Formatting to use f string format

--- commands/nasa_apod.py
+++ commands/nasa_apod.py
@@ -1,6 +1,6 @@
 async def nasa_apod(ctx):
     """ Sends a beautiful space picture of the day from NASA """
-    url = 'https://api.nasa.gov/planetary/apod?api_key=' + NASA_API_KEY
+    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
     response = requests.get(url)
     response = response.json()
 
@@ -9,4 +9,4 @@ async def nasa_apod(ctx):
     photo = response['url']
     title = response['title']
 
-    await ctx.send('** Description: **' + title + "\n" + photo)
+    await ctx.send(f"** Description: **{title}\n{photo}")
