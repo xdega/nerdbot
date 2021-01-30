23cb5e0183ec58af2f0d0bfa8062f313c4009376
Matthew J. DeGarmo <46715299+matthewjdegarmo@users.noreply.github.com>
2021-01-29 21:52:37 -0600

    Function structure refactor (#7)
    * Closes #2
    
    * Splitting command definitions into commands/ folder.
    
    * Initial restructure of commands.
    
    * Formatting name to use f string format
    
    * Formatting to use f string format
    
    * Formatting to use f string format
    
    * Formatting to use f string format
    
    * Formatting spaces in f string.
    
    * Formatting spaces in f string.

---
+++ commands/roll.py
@@ -0,0 +1,5 @@
+async def roll(ctx):
+    """ Rolls a pseudorandom number from 0 to 100 """
+    num = randint(0, 100)
+    num = str(num)
+    await ctx.send(f"{ctx.message.author.mention} rolled {num}")
