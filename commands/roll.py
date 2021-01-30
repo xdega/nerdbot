c8d593d4e0112b53c7d6d209a969f18da4dadae5
Matthew J. DeGarmo <matthewjdegarmo@gmail.com>
2021-01-29 20:54:02 -0600

    Formatting to use f string format

--- commands/roll.py
+++ commands/roll.py
@@ -2,4 +2,4 @@ async def roll(ctx):
     """ Rolls a pseudorandom number from 0 to 100 """
     num = randint(0, 100)
     num = str(num)
-    await ctx.send(ctx.message.author.mention + ' rolled ' + num)
+    await ctx.send(f"{ctx.message.author.mention} rolled {num}")
